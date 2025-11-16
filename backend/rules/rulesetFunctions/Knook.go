d257e1a2166d488637be1c3bfde6fb536787d322
prushton2 <peterrushton418@gmail.com>
2025-11-15 23:10:53 -0500

    knook is back

--- backend/rules/rulesetFunctions/Knook.go
+++ backend/rules/rulesetFunctions/Knook.go
@@ -2,7 +2,9 @@ package rulesetfunctions
 
 import "prushton.com/randochess/v2/board"
 
-func Knook(self board.Board, start int, end int) bool {
-	// return DefaultKnight(self, start, end) || DefaultRook(self, start, end)
-	return true
+func Knook(self board.Board, start int, end int) ([]int, []int) {
+	var knightMoves, knightTakes = DefaultKnight(self, start, end)
+	var rookMoves, rookTakes = DefaultRook(self, start, end)
+
+	return append(knightMoves, rookMoves...), append(knightTakes, rookTakes...)
 }
